9c4a2526ab8b313d20c906c19a417aaa0ce2f95e
Tommaso1699 <69764736+Tommaso1699@users.noreply.github.com>
2023-06-02 23:47:40 +0000

    Update Node_B.py

--- scripts/Node_B.py
+++ scripts/Node_B.py
@@ -1,9 +1,3 @@
-##
-#@package assignment_2_research_track_1
-# \file Node_B.py
-# \brief This file contains a service node that, when called, prints the number of goals reached and cancelled
-# \author Tomasz Strzesak
-
 #!/usr/bin/env python3
 #importing libraries
 import rospy
@@ -11,16 +5,11 @@ from std_srvs.srv import Empty,EmptyResponse
 import assignment_2_2022.msg
 import os
 #global variables
-##
-# reached_goals contains a number of reached goals, which is incremented if goal is reached
+
 reached_goal =0
-##
-# canceled_goals contains a number of canceled goals, which is incremented if goal is canceled
+
 canceled_goal = 0
-##
-# \brief This function is printing reached and canceled goals
-# \param arg is a parameter
-# \return Function returns EmptyResponse()
+
 def Goals(arg): #printing reached and canceled goals
 
 # reached_goals contains a number of reached goals
@@ -28,10 +17,7 @@ def Goals(arg): #printing reached and canceled goals
     global  reached_goal, canceled_goal 
     print("Reached goals: " + str(reached_goal) + " Canceled goals: " + str(canceled_goal) )
     return EmptyResponse()
-##
-# \brief This function is counting reached and canceled goals
-# \param data contains status of goal
-# \return Function is type void
+
 def Counter(data): #counting reached and canceled goals
     if data.status.status == 2:
 
