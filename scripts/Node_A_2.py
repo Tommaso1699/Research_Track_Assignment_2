bf4f5fe00e64f991badb6273aa32ef6a7f798ec2
Tommaso1699 <69764736+Tommaso1699@users.noreply.github.com>
2023-06-02 23:46:01 +0000

    Update Node_A_2.py

--- scripts/Node_A_2.py
+++ scripts/Node_A_2.py
@@ -1,25 +1,14 @@
-##
-#@package assignment_2_research_track_1
-# \file Node_A_2.py
-# \brief This file contains code that publishes the robot position and velocity as a custom message (x,y, vx, vy), by relying on the values published on the topic /odom.
-# \author Tomasz Strzesak
-
 #!/usr/bin/env python3
 #importing libraries
 import rospy
 from nav_msgs.msg import Odometry
 from assignment_2_2022.msg import custom_message_position_and_velocity
 import time
-##
-# \brief This function publishes data about coordinates and velocities
-# \param data contains all information about coordinates and velocities
-# \return This function is type void
+
 def Publish(data): #publishing data about coordinates and velocities
-##
-# publisher publishes received data
+
     publisher = rospy.Publisher('Position_and_velocity', custom_message_position_and_velocity, queue_size = 10) 
-##
-# message takes coordinates and velocities from parameter data
+
     message = custom_message_position_and_velocity()
     print("\nData: \n")
     message.x = data.pose.pose.position.x
